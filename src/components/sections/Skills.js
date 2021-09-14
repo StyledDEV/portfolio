28f76af96d605bfb7d5403fa14562df2409397b2
StyledDEV <apbdev.contact@gmail.com>
2021-09-13 23:49:46 -0300

    adding SingleSkill component

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,35 +1,14 @@
 import React from 'react';
+import SingleSkill from '../SingleSkill';
 
 export default function Skills({ skills }) {
   return (
     <section className="section section-skills">
       <h2 className="section-title">Habilidades</h2>
       <ul className="section-skills-list">
-        {skills.map((skill) => {
-          const { id, url, name } = skill;
-          const skillClass = 'section-skills-list-item';
-
-          return (
-            <li
-              key={id}
-              className={!url ? skillClass : `${skillClass}-container`}
-            >
-              {url ? (
-                <a
-                  className={skillClass}
-                  href={url}
-                  target="_blank"
-                  rel="noreferrer"
-                  title={`Más información de ${name}`}
-                >
-                  {name}
-                </a>
-              ) : (
-                name
-              )}
-            </li>
-          );
-        })}
+        {skills.map((skill) => (
+          <SingleSkill key={skill.id} skill={skill} />
+        ))}
       </ul>
     </section>
   );
